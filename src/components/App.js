a36750ff6da7ad1cfaf30e2f4dc6f2663348beae
williamNyongesa <marakawilliam@gmail.com>
2023-08-16 21:14:10 +0300

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -16,15 +16,23 @@ function Home() {
   );
 }
 
-{/* write an <About> component here */}
+function About(){
+  return (
+    <div id="about">
+      <h2>About</h2>
+    </div>
+  )
+
+  /* write an <About> component here */
+}
 
 
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      {<Home/>/* add the <Home> component here */}
+      {<About/>/* add your <About> component here */}
     </div>
   );
 }
